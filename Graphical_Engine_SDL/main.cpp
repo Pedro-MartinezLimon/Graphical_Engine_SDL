736cd8a44561cb07792115576764f0640af8f58f
= <Pedrolimmon@gmail.com>
2025-06-30 18:39:15 -0600

    feat: Iniciando main

--- Graphical_Engine_SDL/main.cpp
+++ Graphical_Engine_SDL/main.cpp
@@ -1,6 +1,19 @@
 #include "main.hpp"
 
+constexpr int wWindow{ 1280 };
+constexpr int hWindow{ 720 };
+
+SDL_Window* main_window {nullptr};
+
 int main()
 {
+
+
+	if (!SDL_Init(SDL_INIT_VIDEO))
+		SDL_Log("Error loading Window!\n");
+	else
+	{
+		
+	}
 
 }
